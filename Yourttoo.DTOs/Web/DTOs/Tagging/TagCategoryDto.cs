b421019c9908ddd72594961625e657c9e5e992cf
eze <linezekdlr@hotmail.com>
2025-07-31 16:39:49 +0200

    Tagging prototype dto and request class

---
+++ Yourttoo.DTOs/Web/DTOs/Tagging/TagCategoryDto.cs
@@ -0,0 +1,17 @@
+﻿using Yourttoo.DTOs.Base;
+using Yourttoo.DTOs.Common;
+
+namespace Yourttoo.DTOs.Web.DTOs.Tagging
+{
+    public class TagCategoryDto : DtoBase
+    {
+        public string Code { get; set; } = string.Empty;
+        public List<IdiomaticText> Name { get; set; } = new();
+        public List<IdiomaticText> Description { get; set; } = new();
+        public bool Filter { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set;}
+
+        public List<IdiomaticText> Tags { get; set; } = new();
+    }
+}
---
+++ Yourttoo.DTOs/Web/DTOs/Tagging/TagDto.cs
@@ -0,0 +1,21 @@
+﻿using Yourttoo.DTOs.Base;
+using Yourttoo.DTOs.Common;
+
+namespace Yourttoo.DTOs.Web.DTOs.Tagging
+{
+    public class TagDto : DtoBase
+    {
+        public string Code { get; set; } = string.Empty;
+
+        public List<IdiomaticText> Name { get; set; } = new();
+        public List<IdiomaticText> Description { get; set; } = new();
+        public List<IdiomaticText> Label { get; set; } = new();
+
+        public string Slug { get; set; } = string.Empty;
+        public string Status { get; set; } = "active";
+
+        public List<TagCategoryDto> Categories { get; set; } = new();
+
+        public string? Icon { get; set; }
+    }
+}
