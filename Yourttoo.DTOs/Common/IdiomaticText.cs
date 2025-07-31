8fd03ba21fd9db5a79e376a051e7d1169b027a69
eze <linezekdlr@hotmail.com>
2025-07-31 16:39:05 +0200

    Include class for idiomatic multilingual texts

---
+++ Yourttoo.DTOs/Common/IdiomaticText.cs
@@ -0,0 +1,12 @@
+﻿namespace Yourttoo.DTOs.Common
+{
+    /// <summary>
+    /// Represents a text in a specific language.
+    /// 
+    /// </summary>
+    public class IdiomaticText
+    {
+        public string Content { get; set; } = string.Empty;
+        public string Language { get; set; } = string.Empty;
+    }
+}
